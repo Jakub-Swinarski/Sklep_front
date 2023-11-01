fdf3051b00462018b70d05e2ea8e61621dbd543a
bartek <bartekmikiciuk1234@gmail.com>
2023-11-01 12:55:59 +0100

    admin panel styled

--- src/components/resetPassword.vue
+++ src/components/resetPassword.vue
@@ -10,64 +10,43 @@ const token = route.params.token;
 const password = ref()
 const r_password = ref()
 const alertMessage = ref()
+const isVisible = ref(false)
+const isLoading = ref(false)
 const editPassword = () => {
   if (password.value === r_password.value && password.value !== undefined) {
+    isLoading.value = true
+    isVisible.value = false
     UserStore.ResetPasswordEmailSendNewPassword(email, token, password.value)
-        .then(()=>{
+        .then(() => {
           router.push('/login')
-        }).catch((error)=>{
-          alertMessage.value = error.response.data.message;
+        }).catch((error) => {
+      isVisible.value = true
+      isLoading.value = false
+      alertMessage.value = error.response.data.message;
     })
-  }
-  else {
+  } else {
+    isVisible.value = true
     alertMessage.value = "Hasła się nie zgadzają"
   }
 }
-
 </script>
 
 <template>
-  <p>{{ alertMessage }}</p>
-  <p>Nowe hasło</p>
-  <input v-model="password" type="password" name="password" id="password">
-  <p>Powtórz nowe hasło</p>
-  <input v-model="r_password" type="password" name="r_password" id="r_password">
-  <button @click="editPassword">Zapisz</button>
+  <div
+      class="container gap-6 flex flex-col justify-self-center self-center justify-center items-center  text-2xl py-8 ">
+    <p class="text-5xl text-red-700 bg-red-200 px-8 py-4 rounded-xl border-2 border-red-700" v-if="isVisible">
+      {{ alertMessage }}</p>
+    <p>Nowe hasło</p>
+    <input class="min-w-full bg-gray-100 p-2 rounded-full text-center" v-model="password" type="password"
+           name="password" id="password">
+    <p>Powtórz nowe hasło</p>
+    <input class="min-w-full bg-gray-100 p-2 rounded-full text-center" v-model="r_password" type="password"
+           name="r_password" id="r_password">
+    <button class="bg-blue-500 px-4 py-2 rounded-full" v-if="!isLoading" @click="editPassword">Zapisz</button>
+    <img v-if="isLoading" src="@/assets/loader.gif">
+  </div>
 </template>
 
 <style scoped>
-* {
-  box-sizing: border-box
-}
 
-p{
-  font-size: 40px;
-}
-
-h1 {
-  text-align: center;
-  color: #3c0aee;
-  font-size: 70px;
-}
-
-input {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-
-button {
-  background-color: #3c0aee;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-
-}
 </style>
--- src/components/resetPasswordEmail.vue
+++ src/components/resetPasswordEmail.vue
@@ -5,55 +5,36 @@ import UserStore from "@/store/UserStore";
 const email = ref();
 const alertMessage = ref()
 const message = ref()
+const isVisible = ref(false)
+const isLoading = ref(false)
 const reset = () => {
+  isLoading.value = true
+  isVisible.value = false
   UserStore.ResetPasswordEmail(email.value)
       .then((res) => {
         message.value = "Email wysłano"
+        isLoading.value = false
       }).catch((error) => {
-        alertMessage.value = error.response.data.message;
+    isVisible.value = true
+    isLoading.value = false
+    alertMessage.value = error.response.data.message;
   })
 }
 </script>
 
 <template>
-  <h1>Zresetuj hasło</h1>
-  <p>{{ alertMessage }}</p>
-  <input v-model="email" type="email" placeholder="Wpisz adres Email" name="email" id="email" required>
-  <button @click="reset" type="submit" class="button">Wyślij</button>
-  <p>{{ message }}</p>
+  <div
+      class="container gap-6 flex flex-col justify-self-center self-center justify-center items-center  text-2xl py-8 ">
+    <h1>Zresetuj hasło</h1>
+    <p class="text-5xl text-red-700 bg-red-200 px-8 py-4 rounded-xl border-2 border-red-700" v-if="isVisible">{{ alertMessage }}</p>
+    <input class="min-w-full bg-gray-100 p-2 rounded-full text-center" v-model="email" type="email"
+           placeholder="Wpisz adres Email" name="email" id="email" required>
+    <button class="bg-blue-500 px-4 py-2 rounded-full" v-if="!isLoading" @click="reset">Zresetuj</button>
+    <img v-if="isLoading" src="@/assets/loader.gif">
+    <p>{{ message }}</p>
+  </div>
 </template>
 
 <style scoped>
-* {
-  box-sizing: border-box
-}
 
-
-
-h1 {
-  text-align: center;
-  color: #3c0aee;
-  font-size: 70px;
-}
-
-input {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-
-button {
-  background-color: #3c0aee;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-
-}
 </style>
